947e0ccbbfcde775c60f1433d26bc5a39d9084ef
Utkarsh Sangwan <utkarshsangwan7@gmail.com>
2022-02-27 18:55:50 +0530

    \#2 Package

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis-Utkarsh-101903139',         
+  packages = ['Topsis-Utkarsh-101903139'], 
+  version = '0.1',      
+  license='MIT',        
+  description = 'Topsis Package',                   
+  author_email = 'utkarshsangwan7@gmail.com',      
+  url = 'https://github.com/utkarshsangwan7/Topsis-Utkarsh-101903139',  
+  download_url = 'https://github.com/utkarshsangwan7/Topsis-Utkarsh-101903139/archive/refs/tags/v0.1.tar.gz',   
+  keywords = ['TOPSIS'],   
+  install_requires=[            
+          'pandas',
+          'numpy',
+          'copy',
+          'math',
+          'sys'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
